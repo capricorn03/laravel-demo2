81b2d3c24a27e12f2ad257679b259e1346650024
abhi <aasusgod@gmail.com>
2024-08-07 16:48:40 +0530

    chore: Update views and components for job listings and details

--- resources/views/about.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <h1>About Us</h1>
-    <p>Company bio here...</p>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Models\Job;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => Job::all()
+    ]
+    );
 });
+
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
+
+
+
+
 Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
 Route::get('/books/{author}', [BookController::class, 'index'])->name('books.index');
